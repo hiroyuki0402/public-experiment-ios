348fea953947361ed3ce16dda6574e27884e2dc3
hiroyuki0402 <hiroyuki.shiraishi@solodev-hs.com>
2026-04-10 21:31:28 +0900

    feat: AttributeGraphPlayground セットアップ

---
+++ SwiftUI/AttributeGraphPlayground/AttributeGraphPlayground/AttributeGraphPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  AttributeGraphPlaygroundApp.swift
+//  AttributeGraphPlayground
+//
+//  Created by SHIRAISHI HIROYUKI on 2026/04/10.
+//
+
+import SwiftUI
+
+@main
+struct AttributeGraphPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
